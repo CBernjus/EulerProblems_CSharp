60033e21b74f8a494036f9c07ca689335dce269e
Christopher Bernjus <>
2020-04-03 05:48:34 +0200

    solve problem 016 in c#

---
+++ 016 - Power Digit Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace EulerProblems {
+	public static class Program {
+
+		// 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+		// What is the sum of the digits of the number 2^1000?
+
+		public static void Main() {
+
+			BigInteger input = (BigInteger.One << 1000);
+			
+			Console.WriteLine(input);
+			Console.WriteLine(input.ToString().Select(char.GetNumericValue).Sum());
+			Console.ReadLine();
+
+		}
+
+		// Solution: 1366
+
+	}
+}
