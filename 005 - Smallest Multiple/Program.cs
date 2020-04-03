8f178f7fa7bef65659b318b02e386f7f96a797d4
Christopher Bernjus <>
2020-04-03 05:45:52 +0200

    solve problem 005 in c#

---
+++ 005 - Smallest Multiple/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace EulerProblems {
+	public static class Program {
+
+		// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+		// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+		public static long SmallestCommonMultipleOf(int[] numbers) {
+
+			checked {
+
+				for(int i = 0; i < numbers.Length; i++) {
+					if(numbers[i] == 0) return 0;
+					if(numbers[i] < 0) {
+						numbers[i] *= -1;
+					}
+				}
+
+				long lcm = 1;
+				int divisor = 2;
+
+				while(true) {
+					int counter = 0;
+					bool divisible = false;
+
+					for(int i = 0; i < numbers.Length; i++) {
+						if(numbers[i] == 1) counter++;
+
+						if(numbers[i] % divisor == 0) {
+							divisible = true;
+							numbers[i] /= divisor;
+						}
+					}
+
+					if(divisible) {
+						lcm = lcm * divisor;
+					} else {
+						divisor += 1;
+					}
+
+					if(counter == numbers.Length) {
+						return lcm;
+					}
+
+				}
+			}
+
+		}
+
+		public static void Main() {
+
+			const int n = 20;
+			Console.WriteLine(SmallestCommonMultipleOf(Enumerable.Range(1, n).ToArray()));
+			Console.ReadLine();
+
+
+		}
+	}
+}
